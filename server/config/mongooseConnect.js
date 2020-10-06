cec752355ba241bf636a4428d2e450a914b20340
jiaad <abdulziad08@outlook.com>
2020-10-06 12:50:27 +0200

    SomeModifications: to connect Docker

--- server/config/mongooseConnect.js
+++ server/config/mongooseConnect.js
@@ -1,14 +1,21 @@
+/* eslint-disable no-useless-catch */
+import env from 'dotenv'
+import path from 'path'
+
 const mongoose = require('mongoose')
 const asyncHandler = require('../middleware/asyncHandler')
 
+const dbName = 'mongodb://mongo:27017/mernjobs'
+env.config({
+  path: path.resolve(__dirname, '../../.env'),
+})
 const connectDB = asyncHandler(async () => {
-  await mongoose.connect(process.env.MONGO_URI, {
+  await mongoose.connect(dbName, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-
   // mongo console log
-  console.log(`MONGODB CONNECTED SUCCESSFULLY: ${process.env.MONGO_URI}`)
+  console.log(`MONGODB CONNECTED SUCCESSFULLY: ${dbName}`)
 })
 
 module.exports = connectDB
